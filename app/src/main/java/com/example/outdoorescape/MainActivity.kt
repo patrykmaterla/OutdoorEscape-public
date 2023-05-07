410e3eacc135b208fd147d468c79afd7820c0ab5
Patryk Materla <patryk.materla@outlook.com>
2023-05-07 12:17:43 +0200

    Working firebase configuration 2

--- app/src/main/java/com/example/outdoorescape/MainActivity.kt
+++ app/src/main/java/com/example/outdoorescape/MainActivity.kt
@@ -1,11 +1,148 @@
 package com.example.outdoorescape
 
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.CommonStatusCodes
+import com.google.android.gms.signin.internal.SignInClientImpl
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
+    private lateinit var signUpRequest: BeginSignInRequest
+
+
+
+    private val REQ_ONE_TAP = 2
+    private var showOneTapUI = true
+
+//    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+//    private lateinit var firebaseAuth: FirebaseAuth
+//    private lateinit var TAG: String
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        oneTapClient = Identity.getSignInClient(this)
+
+        /*
+        signInRequest = BeginSignInRequest.builder()
+            .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                .setSupported(true)
+                .build())
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.web_client_id))
+                    // Only show accounts previously used to sign in.
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            // Automatically sign in when exactly one credential is retrieved.
+            .setAutoSelectEnabled(true)
+            .build()
+         */
+
+        // Configure the One Tap client for sign up
+        signUpRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    // Your server's client ID, not your Android client ID.
+                    .setServerClientId(getString(R.string.web_client_id))
+                    // Show all accounts on the device.
+                    .setFilterByAuthorizedAccounts(false)
+                    .build())
+            .build()
+
+        // Display the OneTap sign-in UI (if user doesn't have account do nothing i guess)
+        /*
+        oneTapClient.beginSignIn(signInRequest)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0, null)
+                    Toast.makeText(this, "addonscuesfull work", Toast.LENGTH_LONG).show()
+
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                    Toast.makeText(this, "catch", Toast.LENGTH_LONG).show()
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No saved credentials found. Launch the One Tap sign-up flow, or
+                // do nothing and continue presenting the signed-out UI.
+                Log.d(TAG, e.localizedMessage)
+                Toast.makeText(this, "FailFAIL", Toast.LENGTH_LONG).show()
+
+            }
+         */
+
+
+        // Display the One Tap sign-up UI
+        oneTapClient.beginSignIn(signUpRequest)
+            .addOnSuccessListener(this) { result ->
+                try {
+                    startIntentSenderForResult(
+                        result.pendingIntent.intentSender, REQ_ONE_TAP,
+                        null, 0, 0, 0)
+
+                } catch (e: IntentSender.SendIntentException) {
+                    Log.e(TAG, "Couldn't start One Tap UI: ${e.localizedMessage}")
+                }
+            }
+            .addOnFailureListener(this) { e ->
+                // No Google Accounts found. Just continue presenting the signed-out UI.
+                Log.d(TAG, "AddOnFailureListener: ${e.localizedMessage}")
+//                Toast.makeText(this, "signupFAIL", Toast.LENGTH_LONG).show()
+
+            }
+
     }
+
+    // Stop displaying the OneTap UI
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        when (requestCode) {
+//            REQ_ONE_TAP -> {
+//                try {
+//                    // ...
+//                } catch (e: ApiException) {
+//                    when (e.statusCode) {
+//                        CommonStatusCodes.CANCELED -> {
+//                            Log.d(TAG, "One-tap dialog was closed.")
+//                            // Don't re-prompt the user.
+//                            showOneTapUI = false
+//                        }
+//                        CommonStatusCodes.NETWORK_ERROR -> {
+//                            Log.d(TAG, "One-tap encountered a network error.")
+//                            // Try again or just ignore.
+//                        }
+//                        else -> {
+//                            Log.d(
+//                                TAG, "Couldn't get credential from result." +
+//                                        " (${e.localizedMessage})"
+//                            )
+//                        }
+//                    }
+//                }
+//            }
+//
+//        }
+//
+//    }
 }
