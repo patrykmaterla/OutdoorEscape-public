1bff8dfa22efc8a0c6432ede27909e5d303f61c3
Patryk Materla <patryk.materla@outlook.com>
2023-05-17 19:03:42 +0200

    Working email and google sign in

--- app/src/main/java/com/example/outdoorescape/MainActivity.kt
+++ app/src/main/java/com/example/outdoorescape/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.example.outdoorescape
 
+import android.app.Activity
 import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.IntentSender
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.gms.auth.api.identity.BeginSignInRequest
 import com.google.android.gms.auth.api.identity.Identity
 import com.google.android.gms.auth.api.identity.SignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.common.api.CommonStatusCodes
 import com.google.android.gms.signin.internal.SignInClientImpl
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+
+const val REQUEST_CODE_SIGN_IN = 0
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,21 +41,113 @@ class MainActivity : AppCompatActivity() {
     private lateinit var signInRequest: BeginSignInRequest
     private lateinit var signUpRequest: BeginSignInRequest
 
+    private lateinit var firebaseAuth: FirebaseAuth // auth
+
 
 
     private val REQ_ONE_TAP = 2
     private var showOneTapUI = true
 
-//    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
 //    private lateinit var firebaseAuth: FirebaseAuth
 //    private lateinit var TAG: String
+
+
+    private lateinit var btnRegister: Button
+    private lateinit var btnLogin: Button
+    private lateinit var etEmailRegister: EditText
+    private lateinit var etPasswordRegister: EditText
+    private lateinit var tvLoggedIn: TextView
+    private lateinit var etEmailLogin: EditText
+    private lateinit var etPasswordLogin: EditText
+    private lateinit var btnGoogleSignIn: SignInButton
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        oneTapClient = Identity.getSignInClient(this)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        btnRegister = findViewById(R.id.btnRegister)
+        btnLogin = findViewById(R.id.btnLogin)
+        etEmailRegister = findViewById(R.id.etEmailRegister)
+        etPasswordRegister = findViewById(R.id.etPasswordRegister)
+        tvLoggedIn = findViewById(R.id.tvLoggedIn)
+        etEmailLogin = findViewById(R.id.etEmailLogin)
+        etPasswordLogin = findViewById(R.id.etPasswordLogin)
+        btnGoogleSignIn = findViewById(R.id.btnGoogleSignIn)
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            result: ActivityResult ->
+                if (result.resultCode == REQUEST_CODE_SIGN_IN) {
+                    val account = GoogleSignIn.getSignedInAccountFromIntent(result.data).result
+                    account?.let {
+                        googleAuthForFirebase(it)
+                    }
+                }
+        }
+
+
+        /*
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE_SIGN_IN) {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
+            account?.let {
+                googleAuthForFirebase(it)
+            }
+        }
+    }
+ */
+
+
+        btnRegister.setOnClickListener {
+            registerUser()
+        }
+
+        btnLogin.setOnClickListener {
+            loginUser()
+        }
+
+        btnGoogleSignIn.setOnClickListener {
+            val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail()
+                .build()
+            val signInClient = GoogleSignIn.getClient(this, googleSignInOptions)
+            signInClient.signInIntent.also {
+                startActivityForResult(it, REQUEST_CODE_SIGN_IN)
+//                activityResultLauncher.launch(it)
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ---
+
+        // Pobierz konto z wyniku logowania Google One Tap
+        // val signInAccount = GoogleSignIn.getSignedInAccountFromIntent(data).result
+
+// Pobierz idToken z konta Google
+        // val idToken = signInAccount?.idToken
+
+        // val googleAuthProvider = GoogleAuthProvider.getCredential(idToken, null)
+
+//        oneTapClient = Identity.getSignInClient(this)
 
         /*
         signInRequest = BeginSignInRequest.builder()
@@ -54,7 +166,7 @@ class MainActivity : AppCompatActivity() {
             .setAutoSelectEnabled(true)
             .build()
          */
-
+/*
         // Configure the One Tap client for sign up
         signUpRequest = BeginSignInRequest.builder()
             .setGoogleIdTokenRequestOptions(
@@ -66,6 +178,7 @@ class MainActivity : AppCompatActivity() {
                     .setFilterByAuthorizedAccounts(false)
                     .build())
             .build()
+*/
 
         // Display the OneTap sign-in UI (if user doesn't have account do nothing i guess)
         /*
@@ -93,6 +206,7 @@ class MainActivity : AppCompatActivity() {
 
 
         // Display the One Tap sign-up UI
+/*
         oneTapClient.beginSignIn(signUpRequest)
             .addOnSuccessListener(this) { result ->
                 try {
@@ -110,8 +224,88 @@ class MainActivity : AppCompatActivity() {
 //                Toast.makeText(this, "signupFAIL", Toast.LENGTH_LONG).show()
 
             }
+*/
 
     }
+
+    private fun registerUser() {
+        val email = etEmailRegister.text.toString()
+        val password = etPasswordRegister.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main) {
+                        checkLoggedInState()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loginUser() {
+        val email = etEmailLogin.text.toString()
+        val password = etPasswordLogin.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    firebaseAuth.signInWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main) {
+                        checkLoggedInState()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkLoggedInState() {
+        if (firebaseAuth.currentUser == null) { // not logged in
+            tvLoggedIn.text = "You are not logged in"
+        } else {
+            tvLoggedIn.text = "You are logged in!"
+        }
+    }
+
+//    private fun updateProfile() {
+//        val user firebaseAuth.currentUser
+//    }
+
+    private fun googleAuthForFirebase(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                firebaseAuth.signInWithCredential(credentials).await()
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, "Successfully logged in", Toast.LENGTH_LONG).show()
+                }
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == REQUEST_CODE_SIGN_IN) {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
+            account?.let {
+                googleAuthForFirebase(it)
+            }
+        }
+    }
+
+
 
     // Stop displaying the OneTap UI
 //    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
